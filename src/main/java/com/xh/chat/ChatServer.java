31f1cec88166611d1e3d58d011fa359752e54b08
xuhangaa <591765870@qq.com>
2023-12-25 17:29:55 +0800

    108

---
+++ src/main/java/com/xh/chat/ChatServer.java
@@ -0,0 +1,63 @@
+package com.xh.chat;
+
+import com.xh.chat.service.UserServiceFactory;
+import com.xh.message.LoginRequestMessage;
+import com.xh.message.LoginResponseMessage;
+import com.xh.protocol.MessageCodecSharable;
+import com.xh.protocol.ProcotolFrameDecoder;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ChatServer {
+    public static void main(String[] args) {
+        NioEventLoopGroup boss = new NioEventLoopGroup();
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+        LoggingHandler LOGGING_HANDLER = new LoggingHandler(LogLevel.DEBUG);
+        MessageCodecSharable MESSAGE_CODEC = new MessageCodecSharable();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.channel(NioServerSocketChannel.class);
+            serverBootstrap.group(boss, worker);
+            serverBootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel ch) throws Exception {
+                    ch.pipeline().addLast(new ProcotolFrameDecoder());
+                    ch.pipeline().addLast(LOGGING_HANDLER);
+                    ch.pipeline().addLast(MESSAGE_CODEC);
+                    ch.pipeline().addLast(new SimpleChannelInboundHandler<LoginRequestMessage>() {
+                        @Override
+                        protected void channelRead0(ChannelHandlerContext ctx, LoginRequestMessage msg) throws Exception {
+                            String username = msg.getUsername();
+                            String password = msg.getPassword();
+                            boolean login = UserServiceFactory.getUserService().login(username, password);
+                            LoginResponseMessage message;
+                            if(login) {
+                                message = new LoginResponseMessage(true, "登录成功");
+                            } else {
+                                message = new LoginResponseMessage(false, "用户名或密码不正确");
+                            }
+                            ctx.writeAndFlush(message);
+                        }
+                    });
+                }
+            });
+            Channel channel = serverBootstrap.bind(8080).sync().channel();
+            channel.closeFuture().sync();
+        } catch (InterruptedException e) {
+            log.error("server error", e);
+        } finally {
+            boss.shutdownGracefully();
+            worker.shutdownGracefully();
+        }
+    }
+}
---
+++ src/main/java/com/xh/chat/service/HelloService.java
@@ -0,0 +1,5 @@
+package com.xh.chat.service;
+
+public interface HelloService {
+    String sayHello(String name);
+}
