1c4bbe7b5636df25f97c8c0ce0b0d8f19cef5576
xuhangaa <591765870@qq.com>
2024-02-03 10:18:11 +0800

    131

---
+++ src/main/java/com/xh/chat/server/service/HelloService.java
@@ -0,0 +1,5 @@
+package com.xh.chat.server.service;
+
+public interface HelloService {
+    String sayHello(String name);
+}
---
+++ src/main/java/com/xh/p4/TestConnectionTimeout.java
@@ -0,0 +1,33 @@
+package com.xh.p4;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LoggingHandler;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TestConnectionTimeout {
+    public static void main(String[] args) {
+//        new ServerBootstrap().childOption();//socketchannel
+//        new ServerBootstrap().option();//ServerSocketchannel
+        NioEventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap = new Bootstrap()
+                    .group(group)
+                    .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 300)
+                    .channel(NioSocketChannel.class)
+                    .handler(new LoggingHandler());
+            ChannelFuture future = bootstrap.connect("127.0.0.1", 8080);
+            future.sync().channel().closeFuture().sync(); // 断点1
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.debug("timeout");
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+}
